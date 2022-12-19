2b625ae4411c375bc2a317c7cb8cee80ca219063
Zekkkk <1114886299@qq.com>
2022-12-19 23:18:50 +0800

    first day

--- src/day1/B2029.java
+++ src/day1/B2029.java
@@ -1,4 +1,16 @@
 package day1;
 
+import java.util.Scanner;
+
 public class B2029 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int h = in.nextInt();
+        int r = in.nextInt();
+        double v = Math.PI*r*r*h;
+
+        int n = (int)((20/(v/1000))%1==0 ? (20/(v/1000)):(20/(v/1000))+1); // 不是整数就+1
+
+        System.out.println(n);
+    }
 }

482731ffd5f8d0be4f4c23baa65b9a137b7e8190
Zekkkk <1114886299@qq.com>
2022-12-26 11:55:34 +0800

    dp

--- src/day7/P1164.java
+++ src/day7/P1164.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 public class P1164 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int M = in.nextInt();
         int N = in.nextInt();
+        int M = in.nextInt();
 
-        int[] a = new int[N + 1];
+        int[] a = new int[110];
         for (int i = 1; i <= N; i++) {
             a[i] = in.nextInt();
         }
 
-        int[][] dp = new int[N + 1][M + 1];
 
-        for (int i = 0; i <= N; i++) {
-            dp[i][0] = 1;
-        }
+        int[][] dp = new int[110][10010];
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
-                dp[i][j] += dp[i-1][j];
-                if (j >= a[i]) {
-                    dp[i][j] += dp[i-1][j-a[i]];
+                if (a[i] > j) {
+                    dp[i][j] = dp[i-1][j];
+                }
+                if (a[i] == j) {
+                    dp[i][j] = dp[i-1][j] + 1;
+                }
+                if (a[i] < j) {
+                    dp[i][j] = dp[i-1][j-a[i]] + dp[i-1][j];
                 }
             }
         }

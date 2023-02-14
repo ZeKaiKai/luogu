527c5eef4f2d0ebc8fbd850a2b207546ae27159a
Zekkkk <1114886299@qq.com>
2023-02-14 22:33:31 +0800

    递推，斐波那契，大数

--- src/day5/P1255.java
+++ src/day5/P1255.java
@@ -9,16 +9,11 @@ public class P1255 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        BigInteger[] a = new BigInteger[n + 1];
+        BigInteger[] a = new BigInteger[5005];
 
-        a[0] = BigInteger.ZERO;
+        a[1] = BigInteger.ONE;
+        a[2] = new BigInteger("2");
 
-        if (n >= 1){
-            a[1] = BigInteger.ONE;
-        }
-        if (n >= 2){
-            a[2] = new BigInteger("2");
-        }
         if (n >= 3) {
             for (int i = 3; i <= n; i++) {
                 a[i] = a[i - 1].add(a[i - 2]);

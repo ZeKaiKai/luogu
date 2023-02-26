dd2abd7eae2924563606857fbb819f27fc6e89c7
Zekkkk <1114886299@qq.com>
2023-02-26 23:39:45 +0800

    数论

---
+++ src/day29/lq2021_G_最少砝码.java
@@ -0,0 +1,28 @@
+package day29;
+
+import java.util.Scanner;
+
+/**
+ *数论，砝码重量为3^n
+ * 一个砝码 1
+ * 两个砝码 1 3 
+ * 三个砝码 1 3 9
+ * 四个砝码 1 3 9 27
+ * 别被题目提示误导了...
+ */
+public class lq2021_G_最少砝码 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int sum = 0, pow3 = 1, cnt = 0;
+
+        do {
+            sum += pow3;
+            pow3 *= 3;
+            cnt++;
+        } while (sum < num);
+
+        System.out.println(cnt);
+
+    }
+}
